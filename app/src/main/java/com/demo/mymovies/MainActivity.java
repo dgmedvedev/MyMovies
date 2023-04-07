c69f7754f93ff75e7e00f8ef4ea0576e2ae97cd0
dgmedvedev <dg-medvedev@mail.ru>
2023-04-07 15:31:57 +0300

    add NetworkUtils. Minor bugs need to be fixed

--- app/src/main/java/com/demo/mymovies/MainActivity.java
+++ app/src/main/java/com/demo/mymovies/MainActivity.java
@@ -3,6 +3,12 @@ package com.demo.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.demo.mymovies.utils.NetworkUtils;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String url = NetworkUtils.buildURL(NetworkUtils.POPULARITY, 1).toString();
+        Log.i("URL_RESULT", url);
+
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.TOP_RATED,3);
+        if(jsonObject==null){
+            Toast.makeText(this, "Произошла ошибка", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Успешно", Toast.LENGTH_SHORT).show();
+        }
     }
 }
