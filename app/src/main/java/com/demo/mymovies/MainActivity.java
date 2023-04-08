dc33e5e795440e41039700b9d09a089f21560c7d
dgmedvedev <dg-medvedev@mail.ru>
2023-04-08 21:50:21 +0300

    add Movie,JSONUtils,RecyclerView

--- app/src/main/java/com/demo/mymovies/MainActivity.java
+++ app/src/main/java/com/demo/mymovies/MainActivity.java
@@ -1,26 +1,46 @@
 package com.demo.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.demo.mymovies.data.Movie;
+import com.demo.mymovies.utils.JSONUtils;
 import com.demo.mymovies.utils.NetworkUtils;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewPosters;
+    private MovieAdapter movieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        movieAdapter = new MovieAdapter();
 
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.TOP_RATED, 3);
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
         if (jsonObject == null) {
+            Toast.makeText(this, "Проверьте интернет соединение", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "JSON загружен", Toast.LENGTH_SHORT).show();
+        }
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        if (movies.isEmpty()) {
             Toast.makeText(this, "Произошла ошибка", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Успешно", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "ArrayList создан", Toast.LENGTH_SHORT).show();
         }
+        movieAdapter.setMovies(movies);
+        recyclerViewPosters.setAdapter(movieAdapter);
     }
 }
---
+++ app/src/main/java/com/demo/mymovies/data/Movie.java
@@ -0,0 +1,107 @@
+package com.demo.mymovies.data;
+
+public class Movie {
+    private int id;
+    private int voteCount;
+    private double voteAverage;
+    private String title;
+    private String originalTitle;
+    private String overview;
+    private String posterPath;
+    private String bigPosterPath;
+    private String backdropPath;
+    private String releaseDate;
+
+    public Movie(int id, int voteCount, double voteAverage, String title, String originalTitle, String overview, String posterPath, String bigPosterPath,String backdropPath, String releaseDate) {
+        this.id = id;
+        this.voteCount = voteCount;
+        this.voteAverage = voteAverage;
+        this.title = title;
+        this.originalTitle = originalTitle;
+        this.overview = overview;
+        this.posterPath = posterPath;
+        this.bigPosterPath = bigPosterPath;
+        this.backdropPath = backdropPath;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getBigPosterPath() {
+        return bigPosterPath;
+    }
+
+    public void setBigPosterPath(String bigPosterPath) {
+        this.bigPosterPath = bigPosterPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
