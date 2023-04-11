2bdc8c8d23284900fc5f261c3b69706555316006
dgmedvedev <dg-medvedev@mail.ru>
2023-04-11 13:16:22 +0300

    Changed activity_detail.xml

--- app/src/main/java/com/demo/mymovies/DetailActivity.java
+++ app/src/main/java/com/demo/mymovies/DetailActivity.java
@@ -3,12 +3,27 @@ package com.demo.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewBigPoster;
+    private TextView textViewTitle;
+    private TextView textViewOriginalTitle;
+    private TextView textViewRating;
+    private TextView textViewReleaseDate;
+    private TextView textViewOverview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        imageViewBigPoster = findViewById(R.id.imageViewBigPoster);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewOriginalTitle = findViewById(R.id.textViewOriginalTitle);
+        textViewRating = findViewById(R.id.textViewRating);
+        textViewReleaseDate = findViewById(R.id.textViewReleaseDate);
+        textViewOverview = findViewById(R.id.textViewOverview);
     }
 }
